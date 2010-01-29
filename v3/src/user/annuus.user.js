542128d5e83bedd469f199777657dd6307ff6f35
helianthus <project.helianthus@gmail.com>
2010-01-29 03:56:07 +0000

    catch up with 3.99pre
    git-svn-id: http://helianthus-annuus.googlecode.com/svn/trunk@882 28ed74e2-b0a3-11dd-8075-91d349ce83ee

--- v3/src/user/annuus.user.js
+++ v3/src/user/annuus.user.js
@@ -1,14 +1,15 @@
-if(/forum\d+\.hkgolden\.com/.test(location.href))
+(function(encoded)
 {
 	var
 	head = document.getElementsByTagName('head'),
 	script = document.createElement('script');
 
-	script.charset = 'utf-8';
-	script.src = chrome.extension.getURL("annuus.js");
+	//script.charset = 'utf-8';
+	//script.src = 'data:text/javascript;base64,' + encoded;
+	script.appendChild(document.createTextNode(atob(encoded)));
 
 	(function append()
 	{
 		head[0] ? head[0].appendChild(script) : setTimeout(append, 50);
 	})();
-}
+})('${ENCODED_SCRIPT_CONTENT}');
--- v3/src/xpi/content/overlay.js
+++ v3/src/xpi/content/overlay.js
@@ -1,39 +1,40 @@
 window.addEventListener('load', function()
 {
-	var pref = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch).getBranch('extensions.annuus.');  
+	var pref = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch).getBranch('extensions.annuus.');
 	var status = pref.getBoolPref('switchedOn');
-	var eOverlay = document.getElementById('annuus-overlay');
+	var overlay = document.getElementById('annuus-overlay');
 	var rHKGolden = /^http:\/\/forum\d+\.hkgolden\.com/i;
-	
+
 	function setStatus()
 	{
-		eOverlay.setAttribute('status', status ? 'on' : 'off');
+		overlay.setAttribute('status', status ? 'on' : 'off');
 	}
-	
+
 	setStatus();
-	
-	eOverlay.addEventListener('mouseup', function()
+
+	overlay.addEventListener('mouseup', function()
 	{
 		status = !status;
 		setStatus();
 		pref.setBoolPref('switchedOn', status);
 	}, false);
-	
+
 	document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(event)
 	{
 		var doc = event.originalTarget;
 		if(status && doc.nodeName == '#document' && rHKGolden.test(doc.location.href))
 		{
-			var s = doc.createElement('script');
-			s.setAttribute('src', 'resource://annuus/annuus.js');
-			doc.getElementsByTagName('head')[0].appendChild(s);
+			var script = doc.createElement('script');
+			script.charset = 'utf-8';
+			script.src = 'resource://annuus/annuus.js';
+			doc.getElementsByTagName('head')[0].appendChild(script);
 		}
 	}, false);
-	
+
 	gBrowser.addProgressListener({
 		onLocationChange: function(progress, request, uri)
 		{
-			eOverlay.setAttribute('hidden', !(uri && rHKGolden.test(uri.prePath)));
+			overlay.setAttribute('hidden', !(uri && rHKGolden.test(uri.prePath)));
 		},
 		onStateChange: function(){},
 		onProgressChange: function(){},

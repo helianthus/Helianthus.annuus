337598d973697460aeef26d38c5707cb8031b68e
helianthus <project.helianthus@gmail.com>
2010-04-22 15:19:37 +0800

    move some kernel prop as modules

---
+++ src/script/modules/core/credits.js
@@ -0,0 +1,109 @@
+an.addModules(function(){ return {
+
+'2de3e000-9db8-40cf-9fb8-598474ce4df8':
+{
+	title: 'Credit API',
+	pages: { comp: [all] },
+	api: [
+		{
+			type: 'generic',
+			js: function()
+			{
+				var credits = [
+				{
+					name: 'jQuery',
+					url: 'http://jquery.com',
+					author: 'John Resig',
+					authorUrl: 'http://ejohn.org',
+					license: 'Dual licensed under the MIT or GPL Version 2 licenses',
+					licenseUrl: 'http://jquery.org/license'
+				},
+				{
+					name: 'json2.js',
+					url: 'http://www.json.org/js.html',
+					author: 'Douglas Crockford',
+					authorUrl: 'http://www.crockford.com/',
+					license: 'Public Domain'
+				},
+				{
+					name: 'jQuery BBQ',
+					url: 'http://benalman.com/projects/jquery-bbq-plugin/',
+					author: 'Ben Alman',
+					authorUrl: 'http://benalman.com/',
+					license: 'Dual licensed under the MIT and GPL licenses',
+					licenseUrl: 'http://benalman.com/about/license/'
+				},
+				{
+					name: 'parseUri',
+					url: 'http://blog.stevenlevithan.com/archives/parseuri',
+					author: 'Steven Levithan',
+					authorUrl: 'http://blog.stevenlevithan.com/',
+					license: 'MIT License',
+					licenseUrl: 'http://www.opensource.org/licenses/mit-license.php'
+				},
+				{
+					name: 'Apache Ant',
+					url: 'http://ant.apache.org/',
+					author: '[See link]',
+					authorUrl: 'http://ant.apache.org/contributors.html',
+					license: 'Apache License 2.0',
+					licenseUrl: 'http://ant.apache.org/license.html'
+				},
+				{
+					name: 'Closure Compiler',
+					url: 'http://code.google.com/closure/compiler/',
+					author: 'Google',
+					authorUrl: 'http://code.google.com/p/closure-compiler/',
+					license: 'Apache License 2.0',
+					licenseUrl: 'http://www.apache.org/licenses/LICENSE-2.0'
+				},
+				{
+					name: 'base64',
+					url: 'http://www.fourmilab.ch/webtools/base64/',
+					author: 'John Walker',
+					authorUrl: 'http://www.fourmilab.ch/',
+					license: 'Public Domain'
+				},
+				{
+					name: 'crxmake',
+					url: 'http://github.com/Constellation/crxmake',
+					author: 'Constellation',
+					authorUrl: 'http://d.hatena.ne.jp/Constellation/',
+					license: 'MIT License',
+					licenseUrl: 'http://www.opensource.org/licenses/mit-license.php'
+				},
+				{
+					name: 'Red Flower Icon',
+					url: 'http://www.iconarchive.com/show/nature-icons-by-fasticon/Red-Flower-icon.html',
+					author: '[Icons by: FastIcon.com]',
+					authorUrl: 'http://www.fasticon.com',
+					license: 'Fast Icon Commercial License',
+					licenseUrl: 'http://www.fasticon.com/commercial_license.html'
+				},
+				{
+					name: 'Fugue Icons',
+					url: 'http://p.yusukekamiyamane.com/',
+					author: 'Yusuke Kamiyamane',
+					authorUrl: 'http://p.yusukekamiyamane.com/',
+					license: 'Creative Commons Attribution 3.0 license',
+					licenseUrl: 'http://creativecommons.org/licenses/by/3.0/'
+				},
+				{
+					name: 'Still Life',
+					url: 'http://art.gnome.org/themes/icon/1111',
+					author: 'Julian Turner ',
+					license: 'Creative Commons (Attribution-Share Alike 2.0 Generic)',
+					licenseUrl: 'http://creativecommons.org/licenses/by-sa/2.0/'
+				}
+				];
+
+				$.credits = function()
+				{
+					return credits;
+				};
+			}
+		}
+	]
+}
+
+}; });

87d326fdbad7f2c945bcf396e9bbe4fad8ab9610
helianthus <project.helianthus@gmail.com>
2010-05-01 01:12:21 +0800

    Python-like formatting!

--- src/script/kernel/util/jquery.format.js
+++ src/script/kernel/util/jquery.format.js
@@ -1,37 +1,120 @@
 (function($)
 {
-	var
-	convertors = {
+	var radixes = { b:2, o:8, d: 10, x:16, X:16 };
+
+	var convertors = {
 		's': function(target)
 		{
 			return target + '';
-		},
-		'n': function(target)
-		{
-			return target * 1;
 		}
-	},
-	formatters = {
-		'number': function(target, format)
+	};
+
+	$.each(radixes, function(type, radix)
+	{
+		convertors[type] = function(target)
 		{
-			if(format.toLowerCase() === 'x') {
-				target = target.toString(16);
-				if(format === 'X') {
+			return parseInt(target, radix);
+		};
+	});
+
+	var formatters = function(target, format)
+	{
+		format.replace(/^(?:\*(\d+))?(?:(.)?([<>=^]))?([ +-])?(#)?(0)?(\d+)?(,)?(?:.(\d+))?([bcdeEfgGFosxX%])?$/, function($0, repeat, fill, align, sign, _sharp, _0, width, _comma, precision, type)
+		{
+			var undefined;
+			var addSignAtLast = 0;
+
+			if(!type) {
+				type = !$.isNumber(target) ? 's' : 'g';
+			}
+
+			if(type !== 's') {
+				if(sign) {
+					if(sign === '-') {
+						sign = null;
+					}
+					else if(target < 0) {
+						if(sign === ' ') {
+							sign = '-';
+						}
+						else if(sign === '+') {
+							target = -target;
+						}
+					}
+				}
+
+				if(/[bcdoxX]/.test(type)) {
+					target = (target * 1).toFixed(0);
+
+					if(type in radixes) {
+						target = (target * 1).toString(radixes[type]);
+					}
+					else if(type === 'c') {
+						target = String.fromCharCode(target);
+					}
+				}
+				else {
+					if(/e/i.test(type)) {
+						target = target.toExponential(precision || undefined);
+					}
+					else {
+						if(type === '%') {
+							target = target * 100;
+						}
+						target = target[/g/i.test(type) ? 'toPrecision' : 'toFixed'](precision || undefined);
+					}
+				}
+
+				if(/[A-Z]/.test(type)) {
 					target = target.toUpperCase();
 				}
+
+				if(_comma) {
+					target = target.split('.');
+					target = target[0].replace(/\d{3}(?!$)/g, '$&,') + target[1];
+				}
+
+				if(sign) {
+					if(_0 || align === '=') {
+						if(_0) {
+							fill = '0';
+						}
+						align = '>';
+						addSignAtLast = 1;
+					}
+					else {
+						target = sign + target;
+					}
+				}
 			}
-			return target;
-		},
-		'string': function(target, format)
-		{
-			if(format[0] === '*') {
+
+			if(repeat) {
 				var temp = target;
-				for(var i=1; i<format[1]; ++i) {
+				for(var i=1; i<repeat; ++i) {
 					target += temp;
 				}
 			}
-			return target;
-		}
+
+			if(width && align) {
+				var padding = (type === 's' ? Math.max(width, precision) : width) - target.length - addSignAtLast;
+				fill = fill || ' ';
+
+				while(padding-- > 0) {
+					if(align === '<' || align === '^' && padding % 2) {
+						target += fill;
+					}
+					else {
+						target = fill + target;
+					}
+				}
+			}
+
+			if(addSignAtLast) {
+				target = sign + target;
+			}
+		});
+
+		return target;
 	};
 
 	$.format = function(target)
@@ -49,7 +132,7 @@
 
 				if(++count === 10) {
 					$.debug(target, mods);
-					$.err('jQuery.format: too many recursions!');
+					$.error('jQuery.format: too many recursions!');
 				}
 			}
 			while(mods !== temp);
@@ -76,13 +159,13 @@
 				if(convert && convertors[convert]) {
 					replacement = convertors[convert](replacement);
 				}
-				if(format && formatters[typeof replacement]) {
-					replacement = formatters[typeof replacement](replacement, format);
+				if(format) {
+					replacement = formatters(replacement, format);
 				}
 
 				if(!$.isWord(replacement)) {
-					$.debug(target, $0);
-					$.err('jQuery.format: replacement is not a string');
+					$.debug(target, $0, replacement);
+					$.error('jQuery.format: replacement is not a string');
 				}
 
 				return replacement;

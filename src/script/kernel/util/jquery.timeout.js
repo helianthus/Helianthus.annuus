743e5471cc19d3b14a339ee865c801b64c360dd0
helianthus <project.helianthus@gmail.com>
2010-05-15 18:09:05 +0800

    optimize + api change

--- src/script/kernel/util/jquery.timeout.js
+++ src/script/kernel/util/jquery.timeout.js
@@ -2,36 +2,6 @@
 {
 	var cache = {};
 
-	function proxy(id, callback, args)
-	{
-		return function()
-		{
-			if(id) {
-				cache[id].destory = true;
-			}
-
-			callback.apply(null, args);
-
-			if(id && cache[id] && cache[id].destory) {
-				delete cache[id];
-			}
-		};
-	}
-
-	// $.timeout(id);
-	// $.timeout(id, null);
-	// $.timeout(id, args);
-	// $.timeout(id, callback);
-	// $.timeout(id, callback, args);
-	// $.timeout(id, delay);
-	// $.timeout(id, delay, args);
-	// $.timeout(id, delay, callback);
-	// $.timeout(id, delay, callback, args);
-	// $.timeout(delay, callback);
-	// $.timeout(delay, callback, args);
-	// $.timeout(callback);
-	// $.timeout(callback, args);
-
 	$.timeout = function()
 	{
 		var id;
@@ -51,8 +21,21 @@
 		}
 
 		var delay = typeof args[0] === 'number' ? args.shift() : id && cache[id] && cache[id].delay || 0;
-		var callback = $.isFunction(args[0]) ? args.shift() : cache[id].callback;
-		var timer = setTimeout(proxy(id, callback, args[0]), delay);
+		var params = $.isArray(args[0]) ? args.shift() : null;
+		var callback = args[0] || cache[id].callback;
+
+		var timer = setTimeout(function()
+		{
+			if(id) {
+				cache[id].destory = true;
+			}
+
+			callback.apply(null, params);
+
+			if(id && cache[id] && cache[id].destory) {
+				delete cache[id];
+			}
+		}, delay);
 
 		if(id) {
 			cache[id] = {

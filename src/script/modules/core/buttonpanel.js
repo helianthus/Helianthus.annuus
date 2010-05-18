956ff998e75cafff2e8d32d0f8876423391b431b
helianthus <project.helianthus@gmail.com>
2010-05-18 16:34:37 +0800

    much better buttonpanel implementation

--- src/script/modules/core/buttonpanel.js
+++ src/script/modules/core/buttonpanel.js
@@ -10,41 +10,29 @@ annuus.addModules(function(){ return {
 			name: 'button',
 			setup: {
 				css: '\
-					#an-buttonpanel { overflow: hidden; position: fixed; top: 10%; width: 0.1%; height: 80%; } \
-					#an-buttonpanel.show { width: auto; } \
-					#an-buttonpanel > * { box-sizing: border-box; } \
-					#an-buttonpanel > b { display: block; height: .5%; } \
-					#an-buttonpanel > .ui-widget-header { height: 2%; } \
-					#an-buttonpanel > div { position: relative; height: 96%; min-width: 50px; border-left-width: 0; font-size: 75%; } \
-					#an-buttonpanel > div > b { position: absolute; top: 0; bottom: 0; width: 3px; } \
-					#an-buttonpanel > div > .ui-button { display: block; margin: 0 0 3px 0; text-align: left; } \
-					#an-buttonpanel > div > .ui-button > span { padding: 0.1em 1em; } \
+					#an-buttonpanel { position: fixed; top: 10%; height: 80%; min-width: 10px; } \
+					#an-buttonpanel-ui { display: none; box-sizing: border-box; height: 100%; border-top-width: 0.5em; } \
+					#an-buttonpanel-container { overflow: hidden; box-sizing: border-box; height: 100%; min-width: 50px; border-top: 0.2em solid transparent; border-left-width: 5px; font-size: 75%; } \
+					#an-buttonpanel-container > .ui-button { display: block; margin: 0 0 3px 0; text-align: left; } \
+					#an-buttonpanel-container > .ui-button > span { padding: 0.1em 1em; white-space: nowrap; } \
 				',
 				js: function(job)
 				{
-					var working = false;
-					var jPanel = $('\
+					$('\
 						<div id="an-buttonpanel"> \
-							<b class="ui-widget-header"></b> \
-							<b></b> \
-							<div> \
-								<b class="ui-state-default"></b> \
+							<div id="an-buttonpanel-ui" class="an-header-border"> \
+								<div id="an-buttonpanel-container" class="an-default-border"></div> \
 							</div> \
 						</div> \
 					')
-					.hover(function()
+					.hover(function(event)
 					{
-						if(!working) {
-							working = true;
-							jPanel.stop(true, true).hide().css('width', 'auto').show('drop', {}, 500);
-						}
-					}, function()
+						$('#an-buttonpanel-ui')[event.type === 'mouseenter' ? 'filter' : 'not'](':hidden').toggle('fold');
+					})
+					.mousewheel(function(event, delta)
 					{
-						working = false;
-						jPanel.stop(true, true).hide('drop', {}, 500, function()
-						{
-							jPanel.css('width', '').show();
-						});
+						event.preventDefault();
+						$('#an-buttonpanel-container').clearQueue().animate({ scrollTop: $.format('{0}={1}', delta < 0 ? '+' : '-', Math.abs(delta) * 100) }, 'fast', 'linear');
 					})
 					.appendTo('#an');
 				}
@@ -52,7 +40,13 @@ annuus.addModules(function(){ return {
 			add: {
 				js: function(job, options)
 				{
-					$('<a />', { html: options.title }).button().appendTo('#an-buttonpanel > div');
+					var button = $('<a></a>', { html: options.title, href: options.href || '#' }).button().appendTo('#an-buttonpanel-container');
+					if(options.js) {
+						button.click(function()
+						{
+							options.js(options);
+						});
+					}
 				}
 			}
 		},
@@ -61,11 +55,15 @@ annuus.addModules(function(){ return {
 			run_at: 'document_start',
 			js: function()
 			{
-				$.button({
-					title: 'test'
-				});
-				$.button({
-					title: 'longertitle~~~'
+				$.each($.range(1, 40), function(i, val)
+				{
+					$.button({
+						title: 'test button ' + val,
+						js: function()
+						{
+							alert(this.title);
+						}
+					});
 				});
 			}
 		}

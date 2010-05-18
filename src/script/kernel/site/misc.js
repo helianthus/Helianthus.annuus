7587ae0dc234d49a94b7f878f2d2eca2ca4156c5
helianthus <project.helianthus@gmail.com>
2010-05-18 18:53:22 +0800

    block middle click for gecko and webkit

--- src/script/kernel/site/misc.js
+++ src/script/kernel/site/misc.js
@@ -9,4 +9,22 @@ $(document).click(function(event)
 	}
 });
 
+// interestingly Gecko only fires click event for middle click when target is document
+// while Webkit always fires no matter what the target is
+// other browsers in contrast never fires for middle click
+//
+// so we block middle click on click event for consistency
+$.event.special.click = {
+	add: function(details)
+	{
+		var handler = details.handler;
+		details.handler = function(event)
+		{
+			if(event.which === 1) {
+				handler.apply(this, arguments);
+			}
+		};
+	}
+};
+
 $('<div />', { id: 'an' }).appendTo(document.documentElement);

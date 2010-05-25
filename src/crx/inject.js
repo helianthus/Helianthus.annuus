b5ac09f0a109c6d50734caf6b34c5f828295284d
helianthus <project.helianthus@gmail.com>
2010-05-25 11:20:29 +0800

    it turns out there's no need to check for the head in Chrome

--- src/crx/inject.js
+++ src/crx/inject.js
@@ -2,16 +2,10 @@ if(/@PROJECT_TARGET_REGEX@/i.test(location.href))
 {
 	chrome.extension.sendRequest(true, function(status)
 	{
-		if(!status) return;
-
-		var head = document.getElementsByTagName('head');
-		var script = document.createElement('script');
-
-		script.src = chrome.extension.getURL("@PROJECT_NAME_SHORT@.js");
-
-		(function inject()
-		{
-			head[0] ? head[0].appendChild(script) : setTimeout(inject, 50);
-		})();
+		if(status) {
+			var script = document.createElement('script');
+			script.src = chrome.extension.getURL("@PROJECT_NAME_SHORT@.js");
+			document.getElementsByTagName('head')[0].appendChild(script);
+		}
 	});
 }

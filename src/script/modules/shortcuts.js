afaedc8e8dedb5b264ecaf6633187a89be671477
helianthus <project.helianthus@gmail.com>
2010-05-25 11:41:01 +0800

    add shortcuts.js

---
+++ src/script/modules/shortcuts.js
@@ -0,0 +1,17 @@
+annuus.addModules({
+
+'4a263d1b-4c3d-48a0-afc9-a63f13381aa3':
+{
+	title: '加入前往吹水台之按扭',
+	pages: { on: [all] },
+	tasks: {
+		'e6d2ea58': {
+			run_at: 'document_start',
+			ui: ['button'],
+			title: '吹水台',
+			href: '/topics.aspx?type=BW'
+		}
+	}
+}
+
+});

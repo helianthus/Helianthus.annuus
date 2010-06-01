99ba2f464c8ad0e6aa693d9dd3f170fda39bceb3
helianthus <project.helianthus@gmail.com>
2010-06-01 17:25:10 +0800

    follow up resource name change

--- src/script/modules/style.js
+++ src/script/modules/style.js
@@ -122,7 +122,7 @@ annuus.addModules({
 					]
 				}, null, null, function(symbol, i, info)
 				{
-					$.rules('img[src{0}="{1[0]}"] { padding: 0 {1[1]}px {1[2]}px 0; width: 0; height: 0; background: url("{2.resources(icons, {1[3]})}") no-repeat center; }', symbol, info, job);
+					$.rules('img[src{0}="{1[0]}"] { padding: 0 {1[1]}px {1[2]}px 0; width: 0; height: 0; background: url("{2.resources(images, {1[3]})}") no-repeat center; }', symbol, info, job);
 				});
 			}
 		}

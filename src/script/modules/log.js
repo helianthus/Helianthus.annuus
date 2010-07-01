d8d748e9b1549c0a56964ec0c18dff036ded7a36
helianthus <project.helianthus@gmail.com>
2010-07-02 06:59:04 +0800

    initial log panel implementation

---
+++ src/script/modules/log.js
@@ -0,0 +1,43 @@
+annuus.addModules({
+
+'4d8f8a79-93a5-4aa6-b33b-106310687f5e':
+{
+	title: '記錄面板',
+	pages: { on: [all] },
+	tasks: {
+		'e6d2ea58': {
+			ui: 'master',
+			title: '記錄',
+			primary: {
+				css: '\
+					#an-master-log { padding: 1em; } \
+					#an-master-log > li { display: block; font-size: 80%; } \
+					#an-master-log > li > pre { margin: 0; } \
+				',
+				js: function(job)
+				{
+					var panel = $('<ul id="an-master-log" class="ui-helper-reset" />');
+
+					function writeLog(event, type, msg)
+					{
+						panel.prepend($.format(
+							'<li><pre>{1.getHours():02}:{1.getMinutes():02}:{1.getSeconds():02} <span class="an-log-{0}">[{0}] {2}</span></pre></li>',
+							type, new Date(), $.format.apply(null, [msg].concat([].slice.call(arguments, 2))).toString().replace(/</g, '&lt;')
+						));
+					}
+
+					$.each($.log.archives || {}, function(i, data)
+					{
+						writeLog.apply(null, [null].concat(data));
+					});
+
+					$(bolanderi).bind('log', writeLog);
+
+					return panel;
+				}
+			}
+		}
+	}
+}
+
+});

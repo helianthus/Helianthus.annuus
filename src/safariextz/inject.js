314a44e17f661d7267b7d30c8585f264ddec121b
helianthus <project.helianthus@gmail.com>
2010-07-02 07:04:34 +0800

    add safari extension implementation

---
+++ src/safariextz/inject.js
@@ -0,0 +1,11 @@
+if(/@PROJECT_TARGET_REGEX@/i.test(location.href))
+{
+	var script = document.createElement('script');
+	script.src = safari.extension.baseURI + '@PROJECT_NAME_SHORT@.js';
+	var head = document.getElementsByTagName('head');
+
+	(function inject()
+	{
+		head.length ? head[0].appendChild(script) : setTimeout(inject, 50);
+	})();
+}

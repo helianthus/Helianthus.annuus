e94ee393ec0c0a39bf6f09810243fbc4b2adf1c6
helianthus <project.helianthus@gmail.com>
2010-07-02 06:54:57 +0800

    add nameLinks api

--- src/script/modules/core/util/pageelems.js
+++ src/script/modules/core/util/pageelems.js
@@ -3,8 +3,19 @@ annuus.addModules({
 '0f7cdd14-1e7a-4481-81d4-24709102f887':
 {
 	title: 'Page Elements API',
-	pages: { comp: [all] },
+	pages: { comp: [view | topics | search | tags | profilepage] },
 	tasks: {
+		'3adeef77': {
+			type: 'utility',
+			js: function()
+			{
+				$.fn.nameLinks = function()
+				{
+					return $.make(this, '__nameLinks', $(document).pageName === 'view' ? this.find('.repliers_left > div > a') : this.topics().find('a[href^="search"]'));
+				};
+			}
+		},
+
 		'29952b6e': {
 			type: 'utility',
 			page: topics | search | tags | profilepage,
@@ -13,12 +24,12 @@ annuus.addModules({
 				$.fn.extend({
 					topics: function()
 					{
-						return this.__topics || (this.__topics = this.topicTable().find('tr').has('td > a'));
+						return $.make(this, '__topics', this.topicTable().find('tr').has('td > a'));
 					},
 
 					topicTable: function()
 					{
-						return this.__topicTable || (this.__topicTable = this.find({
+						return $.make(this, '__topicTable', this.find({
 							topics: '#HotTopics > div > table',
 							search: '#ctl00_ContentPlaceHolder1_topics_form > table + table > tbody > tr > td > table',
 							tags: '#ctl00_ContentPlaceHolder1_topics_form > table + table > tbody > tr > td > table',
@@ -36,11 +47,7 @@ annuus.addModules({
 			{
 				$.fn.replies = function()
 				{
-					return this.__replies || $.extend((this.__replies = this.find('.repliers')), {
-						jInfos: this.__replies.children().children('tr[userid]'),
-						jNameLinks: this.__replies.find('.repliers_left > div > a'),
-						jContents: this.__replies.find('.repliers_right > tbody > tr:first-child > td')
-					});
+					return $.make(this, '__replies', this.find('.repliers'));
 				};
 			}
 		}

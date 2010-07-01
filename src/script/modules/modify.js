47e5ca3ede8138c3d5eb2b18f268669ac45b330a
helianthus <project.helianthus@gmail.com>
2010-07-02 06:56:46 +0800

    optimize blank name module

--- src/script/modules/modify.js
+++ src/script/modules/modify.js
@@ -40,15 +40,16 @@ annuus.addModules({
 '51ac61dd-9f7a-493e-804a-99acf6b741e4':
 {
 	title: '強制顯示空白會員名稱',
-	pages: { on: [all] },
+	pages: { on: [view | topics | search | tags | profilepage] },
 	tasks: {
 		'475b4b70': {
+			run_at: 'window_load',
 			css: '.an-blankname:before { content: "空白名稱"; font-style: italic; }',
 			js: function(job)
 			{
-				job.context().find('a:visible').filter(function()
+				job.context().nameLinks().filter(function()
 				{
-					return $(this).width() === 0 && /userid=|st=A|ToggleUserDetail/.test($(this).attr('href'));
+					return this.offsetWidth === 0 && /userid=|st=A|ToggleUserDetail/.test($(this).attr('href'));
 				})
 				.addClass('an-blankname');
 			}

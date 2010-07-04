4b89800c42bfac9fe19cc1bfbf0eccb5a7e35d95
helianthus <project.helianthus@gmail.com>
2010-07-04 19:20:30 +0800

    add firebug lite module

---
+++ src/script/modules/firebug.js
@@ -0,0 +1,21 @@
+annuus.addModules({
+
+'730294d8-b68c-4e68-a9b6-ee006813d3f1':
+{
+	title: 'Firebug Lite',
+	pages: { debug: [all] },
+	requires: [
+		{ type: 'truthy', params: !$.browser.mozilla }
+	],
+	tasks: {
+		'e6d2ea58': {
+			run_at: 'document_start',
+			js: function()
+			{
+				$.getScript('https://getfirebug.com/firebug-lite-beta.js');
+			}
+		}
+	}
+}
+
+});

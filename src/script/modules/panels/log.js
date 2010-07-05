b6abbf89c4bcafbbc3b7984379dbeeae57504714
helianthus <project.helianthus@gmail.com>
2010-07-05 10:18:07 +0800

    add padding to beautify log msg a bit

--- src/script/modules/panels/log.js
+++ src/script/modules/panels/log.js
@@ -22,8 +22,8 @@ annuus.addModules({
 					function writeLog(event, type, msg)
 					{
 						panel.prepend($.format(
-							'<li><pre>{1.getHours():02}:{1.getMinutes():02}:{1.getSeconds():02} <span class="an-log-{0}">[{0}] {2}</span></pre></li>',
-							type, new Date(), $.format([msg].concat([].slice.call(arguments, 2))).toString().replace(/</g, '&lt;')
+							'<li><pre>{1.getHours():02}:{1.getMinutes():02}:{1.getSeconds():02} <span class="an-log-{0}">{3:>7} {2}</span></pre></li>',
+							type, new Date(), $.format([msg].concat([].slice.call(arguments, 2))).toString().replace(/</g, '&lt;'), '[{0}]'
 						));
 					}
 

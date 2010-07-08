11e480141b5a13845de5b27ad5896676d0dc2928
helianthus <project.helianthus@gmail.com>
2010-07-09 06:08:59 +0800

    enhance shortcuts module

--- src/script/modules/shortcuts.js
+++ src/script/modules/shortcuts.js
@@ -2,13 +2,28 @@ annuus.addModules({
 
 '4a263d1b-4c3d-48a0-afc9-a63f13381aa3':
 {
-	title: '加入前往吹水台之按扭',
+	title: '快速連結',
 	pages: { on: [all] },
 	tasks: {
 		'e6d2ea58': {
 			service: 'button',
-			title: '吹水台',
-			href: '/topics.aspx?type=BW'
+			widget: function(self)
+			{
+				var widget = $('<div/>');
+
+				$.each({
+					'主論壇頁': '/',
+					'吹水台': '/topics.aspx?type=BW'
+				}, function(text, href)
+				{
+					$('<a/>', {
+						text: text,
+						href: href
+					}).button().appendTo(widget);
+				});
+
+				return widget;
+			}
 		}
 	}
 }

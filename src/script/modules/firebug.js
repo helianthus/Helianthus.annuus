29ff706c588c4a3d391a7d8b1204341e13134a70
helianthus <project.helianthus@gmail.com>
2010-07-10 23:01:24 +0800

    disable firebug

--- src/script/modules/firebug.js
+++ src/script/modules/firebug.js
@@ -3,14 +3,16 @@ annuus.addModules({
 '730294d8-b68c-4e68-a9b6-ee006813d3f1':
 {
 	title: 'Firebug Lite',
-	pages: { debug: [all] },
+	pages: { debug: [0] }, // not helping
 	requires: [!$.browser.mozilla],
 	tasks: {
 		'e6d2ea58': {
 			run_at: 'document_start',
 			js: function()
 			{
-				$.getScript('https://getfirebug.com/firebug-lite-beta.js');
+
+				// firebug has a serious looping bug with safari :(
+				!$.browser.webkit && $.getScript('https://getfirebug.com/firebug-lite-beta.js');
 			}
 		}
 	}

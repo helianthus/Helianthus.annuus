a28662d922b39def2083cb52cf199e8c0cef0a60
helianthus <project.helianthus@gmail.com>
2010-07-26 21:16:48 +0800

    move layout locking as a service

--- src/script/modules/layout.js
+++ src/script/modules/layout.js
@@ -1,40 +1,5 @@
 annuus.addModules({
 
-'86bf4af6-0be0-44e5-a02e-6373776b6982':
-{
-	title: '鎖定表格闊度',
-	pages: { comp: [all] },
-	tasks: {
-		'475b4b70': {
-			run_at: 'document_start',
-			css: '\
-				.repliers > tbody > tr > td + td > table, \
-				.ListPMText > table, .ListPMText > table > tbody > tr > td > table \
-					{ table-layout: fixed; } \
-				.repliers_right td { overflow: hidden; } \
-				body { word-wrap: break-word; } \
-			'
-		},
-
-		'844954b0-d8f3-435d-990b-b6df6460f01e': {
-			run_at: 'document_start',
-			page: view,
-			requires: [!!window.opera],
-			css: '#ctl00_ContentPlaceHolder1_view_form > div > table, .repliers { table-layout: fixed; }'
-		},
-
-		// fix vote page
-		'd594b10a': {
-			page: view,
-			frequency: 'always',
-			js: function(self)
-			{
-				self.context().find('.repliers > tbody > tr > td[colspan="100%"]').attr('colspan', '2');
-			}
-		}
-	}
-},
-
 '3aad9218-46e4-4512-92b8-07d5f5f3626e':
 {
 	title: '縮短Bookmark列',
---
+++ src/script/modules/services/lock.js
@@ -0,0 +1,67 @@
+bolanderi.addModules({
+
+'32c8606b-a331-43ff-bfea-1f6190b7e8a2':
+{
+	title: 'Auto Service (with layout locking)',
+	pages: { on: [all] },
+	tasks: {
+		'192c5c4a-6d3d-4ece-9541-54289c7d4f3e': {
+			type: 'service',
+			name: 'lock',
+			run_at: 'document_start',
+			api: {
+				add: { description: 'add an auto job, after ensuring the layout is locked.', params: ['job'] }
+			},
+			init: function(self, jobs)
+			{
+				$.each(jobs, function(i, job)
+				{
+					self.add(self, job);
+				});
+			},
+
+			isLocked: false,
+
+			lock: function(self)
+			{
+				$.log('log', 'locking layout...');
+
+				$.rules('\
+					.repliers > tbody > tr > td + td > table, \
+					.ListPMText > table, .ListPMText > table > tbody > tr > td > table \
+						{ table-layout: fixed; } \
+					.repliers_right td { overflow: hidden; overflow-x: auto; } \
+					body { word-wrap: break-word; } \
+				');
+
+				if($(document).pageName() === 'view') {
+					window.opera && $.rules('#ctl00_ContentPlaceHolder1_view_form > div > table, .repliers { table-layout: fixed; }');
+
+					// fix vote page
+					$.service.auto.add({
+						frequency: 'always',
+						js: function(self)
+						{
+							self.context().find('.repliers > tbody > tr > td[colspan="100%"]').attr('colspan', '2');
+						}
+					});
+				}
+
+				self.isLocked = true;
+			},
+
+			add: function(self, job)
+			{
+				if(!isLocked) {
+					self.lock(self);
+				}
+
+				$.debug('test');
+
+				$.service.auto.add(job);
+			}
+		}
+	}
+}
+
+});

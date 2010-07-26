a52509ddac89434ea8fa2ab8c253654a2555f7b8
helianthus <project.helianthus@gmail.com>
2010-07-27 01:32:06 +0800

    fix wrong title

--- src/script/modules/shortcuts.js
+++ src/script/modules/shortcuts.js
@@ -120,7 +120,7 @@ annuus.addModules({
 
 '2265ebe2-4d4e-4d85-a8a2-e84f2d6c247f':
 {
-	title: '加入轉換伺服器按扭',
+	title: '加入會員服務按扭',
 	pages: { on: [all] },
 	tasks: {
 		'bd33c014-99bb-4200-989b-45622406737a': {

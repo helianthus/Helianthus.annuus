ada0128365ee6130315a989792d752e006d7b10a
helianthus <project.helianthus@gmail.com>
2010-09-22 00:21:51 +0800

    bolanderi.add now only accepts 1 module at a time

--- src/script/kernel/core/add.js
+++ src/script/kernel/core/add.js
@@ -4,47 +4,42 @@
 var modules = bolanderi.get('MODULES', {});
 var tasks = bolanderi.get('TASKS', {});
 
-bolanderi.add = function(newModules)
+bolanderi.add = function(module)
 {
-	$.each(newModules, function(moduleId, module)
+	if(bolanderi.checkIf.missing(module, ['id', 'title', 'pages', 'tasks'])
+	|| bolanderi.checkIf.exist(modules, module.id)
+	|| $.any(module.tasks, function(taskId)
 	{
-		if(bolanderi.checkIf.exist(modules, moduleId)
-		|| bolanderi.checkIf.missing(module, ['title', 'pages'])
-		|| $.any(module.tasks, function(taskId)
-		{
-			return bolanderi.checkIf.exist(tasks, taskId);
-		})) {
-			return;
-		}
+		return bolanderi.checkIf.exist(tasks, taskId);
+	})) {
+		return;
+	}
 
-		module.uuid = moduleId;
-
-		$.each(module.tasks, function(taskId, task)
-		{
-			$.extend(task, {
-				module: module,
-				uuid: taskId,
-				title: task.title || module.title,
-				type: task.type || 'action'
-			});
-
-			if(task.option) {
-				$.make(module, 'options', taskId, { title: task.option.title, type: 'boolean', defaultValue: task.option.defaultValue });
-				$.make(task, 'condition', 'options', taskId, true);
-			}
-
-			if(task.type === 'service') {
-				$.extend(task, {
-					api: task.api || {},
-					params: task.params || {}
-				});
-			}
-
-			tasks[taskId] = task;
+	$.each(module.tasks, function(taskId, task)
+	{
+		$.extend(task, {
+			module: module,
+			id: taskId,
+			title: task.title || module.title,
+			type: task.type || 'action'
 		});
 
-		modules[moduleId] = module;
+		if(task.option) {
+			$.make(module, 'options', taskId, { title: task.option.title, type: 'boolean', defaultValue: task.option.defaultValue });
+			$.make(task, 'condition', 'options', taskId, true);
+		}
+
+		if(task.type === 'service') {
+			$.extend(task, {
+				api: task.api || {},
+				params: task.params || {}
+			});
+		}
+
+		tasks[taskId] = task;
 	});
+
+	modules[module.id] = module;
 };
 
 })();

f3ff4157f286b00a0613179f16863c932a5299f0
helianthus <project.helianthus@gmail.com>
2010-09-27 06:00:03 +0800

    add channel data

---
+++ src/script/modules/data/channel.js
@@ -0,0 +1,39 @@
+annuus.add({
+	id: '23761c3b-e9d7-411c-8ed9-b7a3d28ee85a',
+	title: 'Channel Data',
+	pages: { comp: [all] },
+	tasks: {
+		'06bff454-073a-4f9f-9e9a-1827f4ab735d': {
+			service: 'data',
+			name: 'channel',
+			json: {
+				ET: '娛樂台',
+				CA: '時事台',
+				FN: '財經台',
+				GM: '遊戲台',
+				HW: '硬件台',
+				IN: '寬頻台',
+				SW: '軟件台',
+				MP: '手機台',
+				SP: '體育台',
+				LV: '感情台',
+				SY: '講故台',
+				ED: '飲食台',
+				TR: '旅遊台',
+				CO: '潮流台',
+				AN: '動漫台',
+				TO: '玩具台',
+				MU: '音樂台',
+				VI: '影視台',
+				DC: '攝影台',
+				ST: '學術台',
+				TS: '汽車台',
+				RA: '電　台',
+				MB: '站務台',
+				AC: '自組活動台',
+				EP: '創意台',
+				BW: '吹水台'
+			}
+		}
+	}
+});

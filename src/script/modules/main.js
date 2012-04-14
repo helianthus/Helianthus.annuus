69a8787946c05a621d48bd84993ebee24e161b24
helianthus <project.helianthus@gmail.com>
2012-04-14 09:55:41 +0000

    update link conversion regex
    git-svn-id: http://helianthus-annuus.googlecode.com/svn/trunk@1145 28ed74e2-b0a3-11dd-8075-91d349ce83ee

--- src/script/modules/main.js
+++ src/script/modules/main.js
@@ -883,7 +883,7 @@ AN.mod['Main Script'] = { ver: 'N/A', author: '向日', fn: {
 	type: 6,
 	once: function()
 	{
-		var rForum = /(?:demoforum|m|forum\d*)\.hkgolden\.com/i;
+		var rForum = /(?:demoforum|groupon|m|forum\d*)\.hkgolden\.com/i;
 		$d.mousedown(function(event)
 		{
 			var jTarget = $(event.target);

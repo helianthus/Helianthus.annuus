3c4319c796a81a2671a41757a9db488ccbcb2b0d
helianthus <project.helianthus@gmail.com>
2012-08-05 12:32:18 +0000

    FileUtils is not needed
    git-svn-id: http://helianthus-annuus.googlecode.com/svn/trunk@1279 28ed74e2-b0a3-11dd-8075-91d349ce83ee

--- src/xpi/lib/resource.js
+++ src/xpi/lib/resource.js
@@ -2,7 +2,6 @@
 {
 	exports.setResource = function(URI)
 	{
-		Cu.import("resource://gre/modules/FileUtils.jsm");
 		Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler).setSubstitution('annuus', URI || null);
 	};
 })();

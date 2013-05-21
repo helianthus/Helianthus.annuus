48422f599789ad523a000afc756a4d37055b3078
helianthus <project.helianthus@gmail.com>
2013-05-21 18:44:14 +0000

    remove username related code in isLoggedIn helper
    git-svn-id: http://helianthus-annuus.googlecode.com/svn/trunk@1353 28ed74e2-b0a3-11dd-8075-91d349ce83ee

--- src/script/kernel/init.js
+++ src/script/kernel/init.js
@@ -645,8 +645,7 @@ $.extend(AN,
 
 		isLoggedIn: function()
 		{
-			AN.util.cookie('username', null, 'hkgolden.com');
-			return !!$('#ctl00_ContentPlaceHolder1_QuickReplyTable').length || !!AN.util.cookie('username');
+			return !!$('#ctl00_ContentPlaceHolder1_QuickReplyTable').length;
 		}
 	},
 

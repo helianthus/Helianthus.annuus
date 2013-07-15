ecdecb680e0055854ce9742f78767a6bdad134c3
helianthus <project.helianthus@gmail.com>
2013-07-15 06:02:07 +0000

    update url filter
    git-svn-id: http://helianthus-annuus.googlecode.com/svn/trunk@1375 28ed74e2-b0a3-11dd-8075-91d349ce83ee

--- src/script/intro.js
+++ src/script/intro.js
@@ -9,7 +9,9 @@ var location = window.location;
 var history = window.history;
 var navigator = window.navigator;
 
-if(!/(?!(?:www|m)\.)[a-z0-9]+\.hkgolden\.com$/i.test(location.hostname) || window.AN) return;
+if(!/(?!(?:www|m|articlehtml)\.)[a-z0-9]+\.hkgolden\.com$/i.test(location.hostname)
+  || /HKGN_top/.test(location.pathname)
+  || window.AN) return;
 
 if(document.body && document.body.firstChild.className == 'webkit-line-gutter-backdrop' || /\.(?:gif|jpe?g|png|asmx)$/i.test(location.href)) return;
 

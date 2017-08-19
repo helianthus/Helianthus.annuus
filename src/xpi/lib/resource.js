d184c6f33fa5fc8d12b2183ec39335603a19de6c
helianthus <project.helianthus@gmail.com>
2017-08-20 01:51:40 +0800

    remove xpi target

--- src/xpi/lib/resource.js
+++
@@ -1,7 +0,0 @@
-(function()
-{
-	exports.setResource = function(URI)
-	{
-		Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler).setSubstitution('annuus', URI || null);
-	};
-})();
